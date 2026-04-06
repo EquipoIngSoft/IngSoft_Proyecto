bc730f6d120782f58aaf124a7949795bfa3bc755
DOCPOOSERSEL <docsersel@gmail.com>
2026-04-06 15:35:43 -0600

    Mejorada la organizacion de las carpetas de css y js

--- resources/views/logIn.blade.php
+++ resources/views/logIn.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600;700&family=Inter:wght@400;500;600&display=swap"
         rel="stylesheet">
     <!-- Hoja de estilos externa -->
-    <link rel="stylesheet" href="{{ asset('css/logIn.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login/logIn.css') }}">
 </head>
 
 <body>
@@ -65,7 +65,7 @@
     </div>
 
     <!-- JS de animaciones -->
-    <script src="{{ asset('animaciones/logIn.js') }}"></script>
+    <script src="{{ asset('animaciones/login/logIn.js') }}"></script>
 
 </body>
 
