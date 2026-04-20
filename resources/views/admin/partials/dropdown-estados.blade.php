e2c75c1757894db3f8364582261ec4f5dbd17a70
Cris <sandovalandrick@gmail.com>
2026-04-19 22:04:49 -0600

    feat: CRUD completo de alumnos, profesores y personal

---
+++ resources/views/admin/partials/dropdown-estados.blade.php
@@ -0,0 +1,41 @@
+<div class="form-dropdown" id="dropdown-{{ $prefix }}-estado" tabindex="0">
+    <div class="form-select-trigger">
+        <span class="selected-text" data-value="">Selecciona un estado</span>
+        <i class="ri-arrow-down-s-line"></i>
+    </div>
+    <div class="form-options-container" style="max-height: 200px; overflow-y: auto;">
+        <div class="form-option" data-value="Aguascalientes">Aguascalientes</div>
+        <div class="form-option" data-value="Baja California">Baja California</div>
+        <div class="form-option" data-value="Baja California Sur">Baja California Sur</div>
+        <div class="form-option" data-value="Campeche">Campeche</div>
+        <div class="form-option" data-value="Chiapas">Chiapas</div>
+        <div class="form-option" data-value="Chihuahua">Chihuahua</div>
+        <div class="form-option" data-value="Ciudad de México">Ciudad de México</div>
+        <div class="form-option" data-value="Coahuila">Coahuila</div>
+        <div class="form-option" data-value="Colima">Colima</div>
+        <div class="form-option" data-value="Durango">Durango</div>
+        <div class="form-option" data-value="Estado de México">Estado de México</div>
+        <div class="form-option" data-value="Guanajuato">Guanajuato</div>
+        <div class="form-option" data-value="Guerrero">Guerrero</div>
+        <div class="form-option" data-value="Hidalgo">Hidalgo</div>
+        <div class="form-option" data-value="Jalisco">Jalisco</div>
+        <div class="form-option" data-value="Michoacán">Michoacán</div>
+        <div class="form-option" data-value="Morelos">Morelos</div>
+        <div class="form-option" data-value="Nayarit">Nayarit</div>
+        <div class="form-option" data-value="Nuevo León">Nuevo León</div>
+        <div class="form-option" data-value="Oaxaca">Oaxaca</div>
+        <div class="form-option" data-value="Puebla">Puebla</div>
+        <div class="form-option" data-value="Querétaro">Querétaro</div>
+        <div class="form-option" data-value="Quintana Roo">Quintana Roo</div>
+        <div class="form-option" data-value="San Luis Potosí">San Luis Potosí</div>
+        <div class="form-option" data-value="Sinaloa">Sinaloa</div>
+        <div class="form-option" data-value="Sonora">Sonora</div>
+        <div class="form-option" data-value="Tabasco">Tabasco</div>
+        <div class="form-option" data-value="Tamaulipas">Tamaulipas</div>
+        <div class="form-option" data-value="Tlaxcala">Tlaxcala</div>
+        <div class="form-option" data-value="Veracruz">Veracruz</div>
+        <div class="form-option" data-value="Yucatán">Yucatán</div>
+        <div class="form-option" data-value="Zacatecas">Zacatecas</div>
+    </div>
+    <input type="hidden" id="{{ $prefix }}-estado" name="{{ $prefix }}_estado" value="">
+</div>
